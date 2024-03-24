b2bf69c49273a2f1990fcc33afcfae7a51d6c023
kiliangui <ichoupette@ik.me>
2024-03-24 20:44:40 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { onMount } from "svelte";
+    import { FFmpeg } from "@ffmpeg/ffmpeg";
+    import { browser } from "$app/environment";
+    let ffmpeg = null;
+    onMount(async ()=>{
+        ffmpeg = new FFmpeg();
+        console.log(ffmpeg);
+        if (browser){
+            await ffmpeg.load();
+        }
+    })
+
+
+
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
